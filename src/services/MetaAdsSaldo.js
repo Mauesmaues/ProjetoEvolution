ecc44699c523a92f30d033e7bf28412ddb100272
Mauesmaues <mauesfullstack@gmail.com>
2025-09-15 14:25:36 -0300

    "Saldo ajustado"

--- src/services/MetaAdsSaldo.js
+++ src/services/MetaAdsSaldo.js
@@ -8,18 +8,35 @@ const ACCESS_TOKEN = process.env.META_ACCESS_TOKEN;
 class MetaAdsSaldo {
     async getSaldo(contaDeAnuncio){
         try{
-            const response = await fetch(`https://graph.facebook.com/v20.0/act_${contaDeAnuncio}?fields=balance&access_token=${ACCESS_TOKEN}`);
+            console.log('[MetaAdsSaldo] Buscando saldo para conta:', contaDeAnuncio);
+            
+            const response = await fetch(`https://graph.facebook.com/v20.0/act_${contaDeAnuncio}?fields=funding_source_details&access_token=${ACCESS_TOKEN}`);
             const data = await response.json();
+            
+            console.log('[MetaAdsSaldo] Resposta da API:', data);
 
             if (data.error) {
                 throw new Error(JSON.stringify(data.error));
             }
 
             let saldo = "nullo";
-            if(data.balance){
-                saldo = data.balance;
+            
+            // Usa apenas funding_source_details
+            if(data.funding_source_details && data.funding_source_details.display_string){
+                console.log('[MetaAdsSaldo] Display string:', data.funding_source_details.display_string);
+                const match = data.funding_source_details.display_string.match(/\(([^)]+)\)/);
+                if(match && match[1]){
+                    saldo = match[1];
+                    console.log('[MetaAdsSaldo] Saldo extraído do display_string:', saldo);
+                } else {
+                    console.log('[MetaAdsSaldo] Não foi possível extrair saldo do display_string');
+                }
+            } else {
+                console.log('[MetaAdsSaldo] funding_source_details não encontrado ou sem display_string');
             }
-            console.log('[MetaAdsSaldo] Saldo obtido:', saldo);
+            
+            console.log('[MetaAdsSaldo] Saldo final:', saldo);
+            
             return new SaldoModel(saldo);
 
         }catch(error){
